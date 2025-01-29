36f48bf713e5292e3dbb5354ebf9277c70472ab7
Melkz Siqueira <melkz.siqueira@gmail.com>
2025-01-28 21:30:22 -0300

    update server port configuration to use port 3000

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
-ENV PORT=8080
-EXPOSE 8080
+ENV PORT=3000
+EXPOSE 3000
 CMD ["npm", "start"]
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import toxicityService from "./services/toxicityService";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 toxicityService
     .initialize()
