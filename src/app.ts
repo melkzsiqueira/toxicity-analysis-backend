6839a405a43afca70533cb59b8404e065ad87c31
Melkz Siqueira <melkz.siqueira@gmail.com>
2025-02-05 04:13:38 -0300

    feat: add request duration logging middleware to track response times

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    const start = Date.now(); // Marca o início da requisição
+
+    res.on("finish", () => {
+        // Evento chamado quando a resposta é enviada
+        const duration = Date.now() - start;
+        console.log(
+            `Requisição para ${req.method} ${req.url} demorou ${duration}ms`
+        );
+    });
+
+    next(); // Passa para o próximo middleware ou rota
+});
 app.post("/analyze-toxicity", toxicityController.analyzeToxicity);
 
 export default app;
